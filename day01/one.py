b6db1c0e822fa45b6c79234a88e2dea22cbe5219
Erik Berta <bertaer@o365.cuni.cz>
2023-12-03 12:10:47 +0100

    day1 + settings

---
+++ day01/one.py
@@ -0,0 +1,40 @@
+""" Day one """
+
+import re
+
+
+def parse_input(filename: str = "input") -> list[str]:
+    with open(filename, encoding="utf-8") as _:
+        return _.read().splitlines()
+
+
+def part_one(lines: list[str]):
+    result = 0
+    for line in lines:
+        digits = re.findall(r"\d", line)
+        result += int(digits[0] + digits[-1])
+    print(result)
+
+
+d = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+
+
+def convert(n):
+    if n in d:
+        return str(d.index(n) + 1)
+    return n
+
+
+def part_two(lines: list[str]):
+    result = 0
+    for line in lines:
+        digits = list(map(convert, re.findall(
+            r'(?=(one|two|three|four|five|six|seven|eight|nine|\d))', line)))
+        result += int(digits[0] + digits[-1])
+    print(result)
+
+
+if __name__ == '__main__':
+    input_lines = parse_input()
+    part_one(input_lines)
+    part_two(input_lines)
