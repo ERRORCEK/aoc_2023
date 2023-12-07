5e01a2cad8af99c2f9af5c54b1e27ef54660f404
Erik Berta <erik.berta@firma.seznam.cz>
2023-12-07 13:17:43 +0100

    day7

---
+++ day07/seven.py
@@ -0,0 +1,68 @@
+""" Day six """
+
+from collections import Counter
+
+
+def parse_input(filename: str = "input") -> list:
+    with open(filename, encoding="utf-8") as _:
+        return _.read().splitlines()
+
+
+def label_to_number(cards, wildcard=False):
+    mapping = {
+        "A": 14,
+        "K": 13,
+        "Q": 12,
+        "J": 1 if wildcard else 11,
+        "T": 10
+    }
+    return [int(mapping.get(card, card)) for card in cards]
+
+
+def get_score(cards, wildcard=False):
+    cards = list(cards)
+
+    jokers = 0
+    if wildcard:
+        jokers = cards.count("J")
+        cards = [card for card in cards if card != "J"]
+
+    counts = list(Counter(cards).values())
+
+    if 5 in counts or jokers == 5:
+        rank_score = 50
+    elif 4 in counts:
+        rank_score = 10 * (4 + jokers)
+    elif 3 in counts and 2 in counts:
+        rank_score = 32
+    elif 3 in counts:
+        rank_score = 10 * (3 + jokers)
+    elif 2 in counts and list(counts).count(2) == 2:
+        rank_score = 22 + 10 * jokers
+    elif 2 in counts:
+        rank_score = 10 * (2 + jokers)
+    else:
+        rank_score = 10 * (1 + jokers)
+
+    return rank_score
+
+
+def calculate_score(lines, wildcard=False):
+    hands = [(label_to_number(hand[0], wildcard=wildcard), int(hand[1]), get_score(hand[0], wildcard=wildcard))
+             for hand in [line.split() for line in lines]]
+    hands = sorted(hands, key=lambda hand: (hand[2], hand[0]))
+    return sum(rank * hand[1] for rank, hand in enumerate(hands, start=1))
+
+
+def part_one(lines: list):
+    print(calculate_score(lines))
+
+
+def part_two(lines: list):
+    print(calculate_score(lines, wildcard=True))
+
+
+if __name__ == '__main__':
+    input_lines = parse_input()
+    part_one(input_lines)
+    part_two(input_lines)
